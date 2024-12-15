3478d2836a9d3171d2db8993342309fed4cda569
mkpoli <git@mkpo.li>
2024-12-16 00:34:00 +0900

    feat: add basic title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="my-10 text-center text-3xl font-bold">Aynu Itah</h1>
