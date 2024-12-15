34d972f5379010d3869bbdd3480c23efc0379e06
mkpoli <git@mkpo.li>
2024-12-16 00:11:38 +0900

    chore: install unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -8,6 +9,9 @@ export default defineConfig({
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
+		}),
+		Icons({
+			compiler: 'svelte'
 		})
 	]
 });
