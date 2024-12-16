e46ec074d09f6af4f3891d8804ba5c3995214116
mkpoli <git@mkpo.li>
2024-12-16 20:19:43 +0900

    feat: add sort method selection

--- src/lib/components/DictionaryTable.svelte
+++ src/lib/components/DictionaryTable.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
 	import * as m from '$lib/paraglide/messages.js';
 	import data from '$lib/data.json';
+
+	let sortBy: 'abc' | 'freq' | 'pos' = $state('freq');
+
+	let sorted = $derived(
+		data.toSorted((a, b) => {
+			if (sortBy === 'abc') {
+				return a.lemma?.localeCompare(b.lemma) ?? 0;
+			} else if (sortBy === 'freq') {
+				return a.frequency - b.frequency;
+			} else if (sortBy === 'pos') {
+				return a.poses?.[0]?.localeCompare(b.poses?.[0]) ?? 0;
+			}
+		})
+	);
 </script>
+
+<form class="mx-auto my-4 flex justify-center gap-2">
+	<label>
+		<span>{m.sort_by()}</span>
+		<select bind:value={sortBy}>
+			<option value="abc">{m.abc()}</option>
+			<option value="freq">{m.freq()}</option>
+			<option value="pos">{m.pos()}</option>
+		</select>
+	</label>
+</form>
 
 <table
 	class="mx-auto max-w-screen-md table-auto border-collapse border border-gray-300 text-center"
@@ -15,7 +40,7 @@
 		</tr>
 	</thead>
 	<tbody>
-		{#each data as item}
+		{#each sorted as item}
 			<tr class="odd:bg-gray-100 even:bg-gray-200">
 				<td class="flex flex-col gap-2">
 					{item.lemma}
