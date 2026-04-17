de038ad7990325c8bd7dd4235031bf18763487f7
mkpoli <git@mkpo.li>
2026-04-17 21:20:02 +0900

    docs: update copyright year

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,7 +7,7 @@
 <hr />
 <footer class="m-2 p-4 text-center">
 	<p class="flex items-center justify-center gap-2">
-		© 2024 <a href="https://aynu.org/" target="_blank">Aynu.org</a>
+		© 2024—2026 <a href="https://aynu.org/" target="_blank">Aynu.org</a>
 		<a href="https://discord.gg/sJRXq3Gspm" target="_blank" title={m.discord()}
 			><Discord class="h-6 w-6" /></a
 		>
